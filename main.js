1e5905b3e0e158be3a47cfd61fcb751f2798a6d1
foxneZz <foxnezz@gmail.com>
2017-06-02 22:34:26 +0300

    added url storage

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-const fs = require("fs")
+const fs = require('fs')
 const express = require('express')
 const bodyParser = require('body-parser');
+const crypto = require('crypto');
 
 const app = express()
-const dataFile = "database/data.txt"
 
+const dataFile = 'database/data.txt'
 const port = 80
 
 app.set('view engine', 'ejs');
@@ -16,12 +17,22 @@ function getIP(req)
     return req.ip.split(':').pop()
 }
 
+function parseData(data)
+{
+    return data.toString().split('\n').filter(a => a.length > 0)
+}
+
+function randomString(length)
+{
+    return crypto.randomBytes(length).toString('hex')
+}
+
 app.get('/', (req, res) =>
 {
     console.log(`${getIP(req)} just accessed /`)
     fs.readFile(dataFile, (err, data) =>
     {
-        res.render('index', {data: data.toString().split('\n').filter(a => a.length > 0)})
+        res.render('index', { data: parseData(data) })
     })
 })
 
@@ -34,14 +45,18 @@ app.post('/add', (req, res) =>
         return
     
     console.log(`Got new text: ${newText}`)
-    fs.appendFile(dataFile, newText + "\n", (err) =>
+
+    var generated = randomString(3)
+
+    fs.appendFile(dataFile, `${generated}:${newText}\n`, (err) =>
     {
-        if (err)
-            throw err
+        if (err) throw err
     })
+
+    res.send(generated)
 })
 
 app.listen(port, () =>
 {
-    console.log('Server running at http://localhost:' + port + '/')
+    console.log(`Server running at http://localhost:${port}/`)
 })
